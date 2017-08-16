061fb0b441b0a87bdc3675ab2a4bf19b1daa52ed
Amir Ben Ari <benari.amir@gmail.com>
2017-08-16 11:43:41 +0300

    creating a process recorder for testing purposes

--- index.js
+++ index.js
@@ -16,10 +16,75 @@ permissions and limitations under the License.
 'use strict';
 
 
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var kcl = require("./lib/kcl/kcl_process");
+var logger = require('./lib/util/logger');
+var log = logger().getLogger('recordProcessor');
+
 /**
- * @fileoverview
- * The main KCL namespace exports user-facing modules and public functions.
- * Note that private modules and private functions may change at a future date without notice.
+ * A simple implementation for the record processor (consumer) that simply writes the data to a log file.
+ *
+ * Be careful not to use the 'stderr'/'stdout'/'console' as log destination since it is used to communicate with the
+ * {https://github.com/awslabs/amazon-kinesis-client/blob/master/src/main/java/com/amazonaws/services/kinesis/multilang/package-info.java MultiLangDaemon}.
  */
 
-module.exports = require("./lib/kcl/kcl_process");
+function recordProcessor() {
+  var shardId;
+
+  return {
+
+    initialize: function(initializeInput, completeCallback) {
+      shardId = initializeInput.shardId;
+
+      completeCallback();
+    },
+
+    processRecords: function(processRecordsInput, completeCallback) {
+      if (!processRecordsInput || !processRecordsInput.records) {
+        completeCallback();
+        return;
+      }
+      var records = processRecordsInput.records;
+      var record, data, sequenceNumber, partitionKey;
+      for (var i = 0 ; i < records.length ; ++i) {
+        record = records[i];
+        data = new Buffer(record.data, 'base64').toString();
+        sequenceNumber = record.sequenceNumber;
+        partitionKey = record.partitionKey;
+        log.info(util.format('ShardID: %s, Record: %s, SeqenceNumber: %s, PartitionKey:%s', shardId, data, sequenceNumber, partitionKey));
+      }
+      if (!sequenceNumber) {
+        completeCallback();
+        return;
+      }
+      // If checkpointing, completeCallback should only be called once checkpoint is complete.
+      processRecordsInput.checkpointer.checkpoint(sequenceNumber, function(err, sequenceNumber) {
+        log.info(util.format('Checkpoint successful. ShardID: %s, SeqenceNumber: %s', shardId, sequenceNumber));
+        completeCallback();
+      });
+    },
+
+    shutdownRequested: function(shutdownRequestedInput, completeCallback) {
+      shutdownRequestedInput.checkpointer.checkpoint(function (err) {
+        completeCallback();
+      });
+    },
+
+    shutdown: function(shutdownInput, completeCallback) {
+      // Checkpoint should only be performed when shutdown reason is TERMINATE.
+      if (shutdownInput.reason !== 'TERMINATE') {
+        completeCallback();
+        return;
+      }
+      // Whenever checkpointing, completeCallback should only be invoked once checkpoint is complete.
+      shutdownInput.checkpointer.checkpoint(function(err) {
+        completeCallback();
+      });
+    }
+  };
+}
+
+log.info('Starting KCL Consumer');
+kcl(recordProcessor()).run();

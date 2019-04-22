1b699c3c938479fdef510d10bf1797351b98eac3
Jacob Peddicord <peddicor@amazon.com>
2019-04-22 14:29:41 -0700

    Relicense to Apache-2.0

--- bin/kcl-bootstrap.bat
+++ bin/kcl-bootstrap.bat
@@ -1,17 +1,7 @@
 @echo off
 
 REM     Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-REM     Licensed under the Amazon Software License (the "License").
-REM     You may not use this file except in compliance with the License.
-REM     A copy of the License is located at
-
-REM     http://aws.amazon.com/asl/
-
-REM     or in the "license" file accompanying this file. This file is distributed
-REM     on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-REM     express or implied. See the License for the specific language governing
-REM     permissions and limitations under the License.
+REM     SPDX-License-Identifier: Apache-2.0
 
 
 node %~dp0\kcl-bootstrap %*

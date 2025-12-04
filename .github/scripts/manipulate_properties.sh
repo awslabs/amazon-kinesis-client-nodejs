de93bb8baf71b7aab2564ff18faca0620530d849
skye rogers <skyero@amazon.com>
2025-12-04 10:53:38 -0800

    refactor variables and file names

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -2,23 +2,21 @@
 set -e
 
 # Copy template files to working locations
-cp .github/resources/sample.properties samples/basic_sample/consumer/sample.properties
+cp .github/resources/github_workflow.properties samples/basic_sample/consumer/github_workflow.properties
 cp .github/resources/config.js samples/basic_sample/producer/config.js
 
-# Manipulate sample.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
+# Manipulate github_workflow.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
 # Depending on the OS, different properties need to be changed
 if [[ "$RUNNER_OS" == "macOS" ]]; then
-  sed -i "" "s/streamName = .*/streamName = $STREAM_NAME/" samples/basic_sample/consumer/sample.properties
-  sed -i "" "s/applicationName = .*/applicationName = $APP_NAME/" samples/basic_sample/consumer/sample.properties
-  sed -i "" "s/stream : 'kclnodejssample'/stream : '$STREAM_NAME'/" samples/basic_sample/producer/config.js
+  sed -i "" "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" samples/basic_sample/consumer/github_workflow.properties
+  sed -i "" "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" samples/basic_sample/consumer/github_workflow.properties
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  sed -i "s/streamName = .*/streamName = $STREAM_NAME/" samples/basic_sample/consumer/sample.properties
-  sed -i "s/applicationName = .*/applicationName = $APP_NAME/" samples/basic_sample/consumer/sample.properties
-  sed -i "s/stream : 'kclnodejssample'/stream : '$STREAM_NAME'/" samples/basic_sample/producer/config.js
+  sed -i "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" samples/basic_sample/consumer/github_workflow.properties
+  sed -i "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" samples/basic_sample/consumer/github_workflow.properties
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
 fi
 
-cat samples/basic_sample/consumer/sample.properties
+cat samples/basic_sample/consumer/github_workflow.properties
 cat samples/basic_sample/producer/config.js
--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -5,11 +5,11 @@ set -o pipefail
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
   cd samples/basic_sample/consumer
-  KCL_COMMAND="../../../bin/kcl-bootstrap --java /usr/bin/java -e -p ./sample.properties"
+  KCL_COMMAND="../../../bin/kcl-bootstrap --java /usr/bin/java -e -p ./github_workflow.properties"
   gtimeout $RUN_TIME_SECONDS $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
   cd samples/basic_sample/consumer
-  KCL_COMMAND="../../../bin/kcl-bootstrap -e -p ./sample.properties"
+  KCL_COMMAND="../../../bin/kcl-bootstrap -e -p ./github_workflow.properties"
   timeout $RUN_TIME_SECONDS $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
 else
   echo "Unknown OS: $RUNNER_OS"
